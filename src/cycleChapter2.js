46f803f53787da9b3e41d5d9d1f0aa9a3811f183
Victoria Nazarova <nika_mirai@mail.ru>
2025-12-05 06:30:19 +0400

    Самое популярное имя

--- src/cycleChapter2.js
+++ src/cycleChapter2.js
@@ -10,3 +10,47 @@ const reverse = (str) => {
 };
 
 reverse('): !модог мывон с');
+
+/*
+Задание 3. Напишите ф-цию, которая принимает на вход массив пользователей,
+а возвращает наиболее популярное имя среди совершеннолетних пользователей
+ */
+
+const mostPopularAdultName = (users) => {
+  const people = {};
+  for (const user of users) {
+    if (people[user.name] && user.age >= 18) {
+      people[user.name]++;
+    } else if (user.age >= 18) {
+      people[user.name] = 1;
+    }
+  }
+  let popName;
+  let kol = 0;
+  for (const mostMen in people) {
+    if (kol < people[mostMen]) {
+      popName = mostMen;
+      kol = people[mostMen];
+    }
+  }
+  return popName;
+};
+
+const users = [
+  { name: 'ivan', age: 19 },
+  { name: 'olga', age: 1 },
+  { name: 'alex', age: 22 },
+  { name: 'maria', age: 17 },
+  { name: 'ivan', age: 25 },
+  { name: 'olga', age: 30 },
+  { name: 'alex', age: 18 },
+  { name: 'sergey', age: 20 },
+  { name: 'maria', age: 21 },
+  { name: 'ivan', age: 16 },
+  { name: 'olga', age: 12 },
+  { name: 'ivan', age: 23 },
+  { name: 'olga', age: 17 },
+];
+
+const result = mostPopularAdultName(users);
+console.log(result); // ivan
